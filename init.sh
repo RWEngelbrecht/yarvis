440c85877fb6ed78381a09e847c682d1a8c89f66
rigardt.engelbrecht <rigardt.engelbrecht@global.ntt>
2021-10-26 07:34:52 +0200

    bash scripts are finicky. also cannot execute source, i guess?

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 #compile yarvis
 sudo go build -o /bin/yarvis .
 
@@ -7,16 +7,22 @@ ZSH_RC=~/.zshrc
 BASH_RC=~/.bashrc
 if [ ! -f "$ZSH_RC" ]; then
   touch .zshrc
-elif [ ! -f "$BASH_RC" ]; then
+fi
+if [ ! -f "$BASH_RC" ]; then
   touch .bashrc
 fi
 
-if ! grep -Fxq "alias yarvis" $ZSH_RC && ! grep -Fxq "alias yarvis" $BASH_RC; then
-  if [ -f "$ZSH_RC" ]; then
-    echo 'alias yarvis="/bin/yarvis"' >> $ZSH_RC
-    
-  elif [ -f "$BASH_RC" ]; then
-    echo 'alias yarvis="/bin/yarvis"' >> $BASH_RC
-    source $BASH_RC
-  fi
+# if no new line at end of rc file, this will break
+if ! grep -Fq "alias yarvis" $ZSH_RC; then
+  echo "Alias not found, adding to $ZSH_RC...\n"
+  echo 'alias yarvis="/bin/yarvis"' >> $ZSH_RC
 fi
+
+if ! grep -Fq "alias yarvis" $BASH_RC; then
+  echo "Alias not found, adding to $BASH_RC...\n"
+  echo 'alias yarvis="/bin/yarvis"' >> $BASH_RC
+fi
+
+echo "ACTION REQUIRED: \
+\nFor these changes to take effect, run: \
+\n\t'source <.*rc_file>'"
